1d03008aa0a89e2a2a7462af04846135abae21c6
cutema <cutemalinfei@outlook.com>
2022-08-01 20:08:46 +0800

    第一次提交

---
+++ input.py
@@ -0,0 +1,59 @@
+# 主要学习使用input()输入方法
+if __name__ == '__main__':
+    # string = input("请输入一个字符串用于测试：")
+    # print(string)
+    #
+    # num = input("请输入一个整数用于测试：")
+    # print(type(num))
+    # print(int(num))
+    #
+    # num_f = input("请输入一个浮点数用于测试")
+    # print(type(num_f))
+    # print(float(num_f))
+    # 类型转换
+    # string = input("请输入：")
+    # string = float(string)
+    # a = string + 13.45
+    # print(type(string), type(a))1
+    # print(a, string)
+
+    # print(chr(2))
+    # print(ord('1'))
+    # print(hex(13))
+    # print(oct(8))
+    # print(bin(2))
+    #
+    # a = 10
+    # print(type(a))
+    # print(type(float(a)))
+    #
+    # str1 = '10'
+    # str2 = '10.88'
+    # print(type(eval(str1)))
+    # print(type(eval(str2)))
+    #
+    # name = input("请输入您购买商品名称：")
+    # item_id = int(input("请输入您购买商品编号："))
+    # price = float(input("请输入您购买商品价格："))
+    #
+    # print("您购买了%s,商品编号为%s,商品价格为%.2f,欢迎下次光临！" % (name, item_id, price))
+
+    # a = 2
+    # b = 3
+    # h = 5
+    # s = (a + b) * h / 2
+    # print(s)
+    # print(1 + 1)
+    # print(1 - 1)
+    # print(1 * 1)
+    # print(1 / 1)
+    # print(1 // 1)
+    # print(3 % 2)
+    # print(2 ** 2)
+    # print((2 ** 2) / 3 - 3)
+    #
+
+    a = 1
+    b = 2
+    c = a if a > b else b
+    print(c)
